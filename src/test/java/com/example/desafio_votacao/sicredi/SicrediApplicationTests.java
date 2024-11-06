fa8da5eedc5859b15b77d6d9fe7ead7fc0cc18d1
lucas-brunkhorst <lucasbrunkhorstrosas@gmail.com>
2024-11-06 18:44:17 -0300

    Initial project setup with Spring Boot

---
+++ src/test/java/com/example/desafio_votacao/sicredi/SicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_votacao.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
